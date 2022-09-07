0a561ecf504cb9b06f101663f6a0de872d4d5c7e
takayuki-takahashi-dic <ttkaws15@gmail.com>
2022-09-07 20:58:04 +0900

    【portfolio_00】FV（タイピングアニメーション追加）

---
+++ portfolio_00/_static/dist/js/index.js
@@ -0,0 +1,13 @@
+$(function() {
+  $('.typed').typed({
+    strings: ["タカハシタカユキのポートフォリオ"],
+    // strings: ["タカハシタカユキのポートフォリオ", "左の画像をクリック"],
+    typeSpeed: 100,
+    startDelay: 1000,
+    backSpeed: 20,
+    loop: false,
+    loopCount: Infinity,
+    showCursor: false,
+    backDelay: 500
+  });
+});
