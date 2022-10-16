813aa20e0df3e0cba177398c6b31e7361623ff5c
takayuki-takahashi-dic <ttkaws15@gmail.com>
2022-10-16 20:04:09 +0900

    【PF_01】first commit

---
+++ PF_01/_static/dist/js/main.js
@@ -0,0 +1,11 @@
+// MicroModal.init();
+
+MicroModal.init({
+  disableScroll: false, // モーダルが開いている間に、背景スクロールを無効にする。
+});
+
+//   // モーダルを開く
+//   MicroModal.show('モーダルのid属性値');
+
+//   // モーダルを閉じる
+//   MicroModal.close('モーダルのid属性値');
